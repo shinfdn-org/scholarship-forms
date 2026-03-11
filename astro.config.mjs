09786743c4abf5106135eab4349e4c6f1348da88
Tomoki Saito | 齋藤 智樹 <53966963+Tomoki-Saito@users.noreply.github.com>
2026-03-11 18:56:01 +0900

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://shinfdn-org.github.io',
+  base: '/scholarship-forms',
+  build: { assets: 'assets' },
+});
