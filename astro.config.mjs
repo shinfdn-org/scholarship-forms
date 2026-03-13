5027699d119a70f88f34b7eb3a3a482f4ccba209
Tomoki Saito | 齋藤 智樹 <53966963+Tomoki-Saito@users.noreply.github.com>
2026-03-13 10:17:30 +0900

    ドメインを forms.shinfdn.org に変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://forms.shinfdn.org',
-  base: '/scholarship-forms',
+  base: '/',
   build: { assets: 'assets' },
 });
